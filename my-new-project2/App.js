89c912dd7d614c4bd34f6415d2d572b647d103c3
Hallur20 <hvn15@hotmail.com>
2017-10-27 15:01:53 +0200

    push

---
+++ my-new-project2/App.js
@@ -0,0 +1,241 @@
+import React from 'react';
+import { FlatList, TouchableHighlight, Alert, TextInput, ScrollView, Image, Text, View, Platform, TouchableOpacity, StyleSheet, Button, WebView } from 'react-native';
+import { Constants, WebBrowser } from "expo";
+import { StackNavigator } from 'react-navigation';
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+//Todo: Refactor into a seperate file (Basics.js), import and complete the exercise
+class Basics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  
+  _onLongPressButton() {
+    Alert.alert('You long-pressed the button!')
+  }
+
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (<ScrollView><Text>basics 1:Hello World!</Text><Text>basics2:</Text><Image source={pic} style={{width: 193, height: 110}}/><Text>Hello {this.props.title}!</Text>
+    <Greeting name='Rexxar' />
+    <Greeting name='Jaina' />
+    <Greeting name='Valeera' />
+    <Text>basics 3:</Text>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+        <Text>basics 4:</Text>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigbluee</Text>
+        <Text>basics 5:</Text>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+        <Text>basics 6: </Text>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+      <Text>basics 6:</Text>
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+      <Text>basics 7:</Text>
+      <Button
+  onPress={() => { Alert.alert('You tapped the button!')}}
+  title="Press Me"
+/>
+<TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+          </View>
+        </TouchableHighlight>
+        <Text>basics 8: (this whole page is a scrollview.</Text>
+          <Text>basics 9:</Text>
+          <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
+    </ScrollView>)
+  }
+}
+
+
+
+//Todo: Refactor into a seperate file (Props.js), import and complete the exercise
+class Props extends React.Component {
+  static navigationOptions = { title: "Learn about Props" }
+  render() {
+    return (
+      <View>
+        <Text>Props</Text>
+      </View>
+    )
+  }
+}
+//Todo: Create a new file (State.js), import it, add a "Touchable", and complete the exercise
+//Todo: Create a new file (Style.js), import it, add a "Touchable", and complete the exercise
+//Todo: Create a new file (HeightWidth.js), import it,add a "Touchable", and complete the exercise
+//Todo: Create a new file (HeightWidth.js), import it,add a "Touchable", and complete the exercise
+//Todo: Create a new file (FlexBox.js), import it,add a "Touchable", and complete the exercise
+//Todo: Create a new file (TextInput.js), import it,add a "Touchable", and complete the exercise
+//Todo: Create a new file (Touches.js), import it,add a "Touchable", and complete the exercise
+//Todo: Create a new file (ScrollView.js), import it,add a "Touchable", and complete the exercise
+//Todo: Create a new file (ListViews.js), import it,add a "Touchable", and complete the exercise
+//Todo: Create a new file (NetWorking.js), import it,add a "Touchable", and complete the exercise
+
+const Touchable = (props) => (
+  <TouchableOpacity style={styles.button} onPress={props.onPress}>
+    <Text style={styles.buttonText}>{props.title}</Text>
+  </TouchableOpacity>)
+
+//Todo: Refactor into a seperate file (WhatToDo.js), import and complete the exercise
+class WhatToDo extends React.Component {
+  static navigationOptions = { title: "What I have to do" }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ marginBottom: 3 }}>Complete all steps in Facebooks "The Basics" tutorial (see below)</Text>
+        <Text style={{ marginBottom: 3 }}>For each of the 11 steps, add a corresponding Component in this project + the necessecary changes to navigate into it.
+        (inspired of how you navigated into this)
+      </Text>
+        <WebView
+          source={{ uri: "https://facebook.github.io/react-native/docs/tutorial.html" }}
+          style={{ marginTop: 20, flex: 1 }}
+        />
+      </View>
+    );
+  }
+}
+
+
+class HomeScreen extends React.Component {
+  static navigationOptions = { title: 'Day1 Tutorial' };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View >
+        <Text style={{ textAlign: "center", fontSize: 20 }}>See all Demos implemented by XXXX</Text>
+        <Touchable onPress={() => navigate('web')} title="What I have to do" />
+        <Touchable onPress={() => navigate('basics')} title="Basics" />
+        <Touchable onPress={() => navigate('props')} title="Props" />
+      </View>
+    )
+  }
+}
+
+export default App = () => <RouteStack style={{ marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight / 2 }} />
+
+const RouteStack = StackNavigator({
+  Home: { screen: HomeScreen },
+  basics: { screen: Basics },
+  props: { screen: Props },
+  web: { screen: WhatToDo },
+});
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 3,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 7,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: 'white'
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  container: {
+    flex: 1,
+    paddingTop: 22
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
+})
